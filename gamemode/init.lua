54da208aeb93ffbb03e3eff3b1efd6f5a29d2f74
ArmageddonScr <armageddonscr@gmail.com>
2014-12-21 19:26:01 -0500

    Server/Share Reverse Fix
    round 2

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -3,16 +3,10 @@ AddCSLuaFile("shared.lua")
 
 include ("shared.lua")
 
-<<<<<<< HEAD
 function GM:PlayerSpawn(ply)
 	local a = team.BestAutoJoinTeam()
 	local b = team.GetName(a)
 	ply:SetTeam(a)
 	print( ply:Nick() .. " joined team " .. b )
 end
-=======
-function GM:Initialize()
-	team.SetUp(1,"Red",Color(50,0,0),true)
-	team.SetUp(2,"Gold",Color(50,50,0),true)
-end
->>>>>>> origin/master
+
