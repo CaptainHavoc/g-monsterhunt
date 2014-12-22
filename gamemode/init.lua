5780117f822763f94e76cecda8e6dbcb5bcf0288
ArmageddonScr <armageddonscr@gmail.com>
2014-12-21 19:21:30 -0500

    Server/Share Fix
    The code for init.lua and shared.lua was resersed.

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -2,3 +2,10 @@ AddCSLuaFile("cl_init.lua")
 AddCSLuaFile("shared.lua")
 
 include ("shared.lua")
+
+function GM:PlayerSpawn(ply)
+	local a = team.BestAutoJoinTeam()
+	local b = team.GetName(a)
+	ply:SetTeam(a)
+	print( ply:Nick() .. " joined team " .. b )
+end
--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -7,11 +7,3 @@ function GM:Initialize()
 	team.SetUp(1,"Red",Color(50,0,0),true)
 	team.SetUp(2,"Gold",Color(50,50,0),true)
 end
-
-function GM:PlayerSpawn(ply)
-	local a = team.BestAutoJoinTeam()
-	local b = team.GetName(a)
-	ply:SetTeam(a)
-	print( ply:Nick() .. " joined team " .. b )
-end
-	
