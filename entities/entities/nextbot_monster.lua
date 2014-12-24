a6c2b3ebf08c7f99d7d43a18e66981163feb6ee2
ArmageddonScr <armageddonscr@gmail.com>
2014-12-24 16:15:30 -0500

    Generic Update #1 -B
    Fixed test npc not registering due to folders not being setup properly.
    added a command to spawn npc. testing and debugging ai to learn nextbot.

--- entities/entities/nextbot_monster.lua
+++ entities/entities/nextbot_monster.lua
@@ -1,7 +1,7 @@
-AddCSLuaFile()
+AddCSLuaFile("nextbot_monster.lua")
 
-ENT.Base		="base_nextbot"
-ENT.Spawnable	=true
+ENT.Base				= "base_nextbot"
+ENT.Spawnable		= true
 
 function ENT:Initialize()
 	self:SetModel("models/zombie/classic.mdl")
@@ -30,6 +30,8 @@ end
 function ENT:RunBehaviour()
 	-- This function is called when the entity is first spawned, it acts as a giant loop that will run as long as the NPC exists
 	while ( true ) do
+		
+		
 		-- Lets use the above mentioned functions to see if ( we have/can find a enemy
 		if ( self:HaveEnemy() ) then
 			-- Now that we have a enemy, the code in this block will run
@@ -38,7 +40,7 @@ function ENT:RunBehaviour()
 			self:PlaySequenceAndWait( "hunter_angry" )-- Play an animation to show the enemy we are angry
 			self:PlaySequenceAndWait( "unplant" )	-- Get out of the pose
 			self:StartActivity( ACT_RUN )			-- Set the animation
-			self.loco:SetDesiredSpeed( 450 )		-- Set the speed that we will be moving at. Don't worry, the animation will speed up/slow down to match
+			self.loco:SetDesiredSpeed( 170 )		-- Set the speed that we will be moving at. Don't worry, the animation will speed up/slow down to match
 			self.loco:SetAcceleration( 900 )			-- We are going to run at the enemy quickly, so we want to accelerate really fast
 			self:ChaseEnemy() 						-- The new function like MoveToPos that will be looked at soon.
 			self.loco:SetAcceleration( 400 )			-- Set this back to its default since we are done chasing the enemy
@@ -46,17 +48,18 @@ function ENT:RunBehaviour()
 			self:StartActivity( ACT_IDLE )			--We are done so go back to idle
 			-- Now once the above function is finished doing what it needs to do, the code will loop back to the start
 			-- unless you put stuff after the if ( statement. Then that will be run before it loops
+			
 		else
 			-- Since we can't find an enemy, lets wander
 			-- Its the same code used in Garry's test bot
 			self:StartActivity( ACT_WALK )			-- Walk anmimation
-			self.loco:SetDesiredSpeed( 200 )		-- Walk speed
+			self.loco:SetDesiredSpeed( 100 )		-- Walk speed
 			self:MoveToPos( self:GetPos() + Vector( math.Rand( -1, 1 ), math.Rand( -1, 1 ), 0 ) * 400 ) -- Walk to a random place within about 400 units ( yielding )
 			self:StartActivity( ACT_IDLE )
 		end
 		-- At this point in the code the bot has stopped chasing the player or finished walking to a random spot
 		-- Using this next function we are going to wait 2 seconds until we go ahead and repeat it 
-		coroutine.wait( 2 )
+		coroutine.wait( 1 )
 		
 	end
 
@@ -80,7 +83,7 @@ function ENT:ChaseEnemy( options )
 		end
 		path:Update( self )								-- This function moves the bot along the path
 		
-		if ( options.draw ) then path:Draw() end
+		if not ( options.draw ) then path:Draw() end
 		-- If we're stuck ) then call the HandleStuck function and abandon
 		if ( self.loco:IsStuck() ) then
 			self:HandleStuck()
@@ -95,8 +98,12 @@ function ENT:ChaseEnemy( options )
 
 end
 
+function ENT:Use()
+	self:Remove()
+end
+
 list.Set( "NPC", "simple_nextbot", {
-	Name = "Simple bot", 
+	Name = "Simple Bot", 
 	Class = "simple_nextbot", 
 	Category = "NextBot"
 } )
--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -1,10 +1,22 @@
 GM.Name 	= "Monster Hunt"
 GM.Author 	= "Captain Havoc and ArmageddonScr"
-GM.Email 		= "N/A"
+GM.Email 	= "N/A"
 GM.Website 	= "N/A"
+
+mh={}
 
 function GM:Initialize()
 	team.SetUp(1,"Red",Color(50,0,0),true)
 	team.SetUp(2,"Gold",Color(50,50,0),true)
 end
 
+function mh.CreateNpc(ply,cmd,args,fullstr)
+	if ( CLIENT ) then return end
+	local npc=args[1]
+	local ent=ents.Create(npc or "nextbot_monster")
+	ent:Activate()
+	ent:Spawn()
+	
+	ent:SetPos( ply:GetEyeTrace().HitPos )
+end
+concommand.Add( "mh_spawn_npc", mh.CreateNpc )
