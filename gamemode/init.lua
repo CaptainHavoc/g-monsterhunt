693a306fc46d9d1567a62a1f5467abc4b0b16893
ArmageddonScr <armageddonscr@gmail.com>
2014-12-24 19:57:58 -0500

    Minor Generic Update #1 -B
    Models + A weapon.

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -8,5 +8,7 @@ function GM:PlayerSpawn(ply)
 	local b = team.GetName(a)
 	ply:SetTeam(a)
 	print( ply:Nick() .. " joined team " .. b )
+	ply:SetModel("models/player/group03/male_0" .. math.random(1,9) .. ".mdl")
+	ply:Give("cw_bizongsm")
 end
 
