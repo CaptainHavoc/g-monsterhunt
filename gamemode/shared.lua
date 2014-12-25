6bc381396b9c6834ee5b49f37e4e9dfb5ab49d7a
Captain Havoc <garandcarbine@gmail.om>
2014-12-24 19:05:43 -0500

    Mini-update #1 -C
    Changed team colors to be less sucky. Cleaned up code slightly so it's a
    bit easier to read.

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -6,17 +6,17 @@ GM.Website 	= "N/A"
 mh={}
 
 function GM:Initialize()
-	team.SetUp(1,"Red",Color(50,0,0),true)
-	team.SetUp(2,"Gold",Color(50,50,0),true)
+	team.SetUp(1,"Red",Color(187,0,0),true)
+	team.SetUp(2,"Gold",Color(255,201,14),true)
 end
 
 function mh.CreateNpc(ply,cmd,args,fullstr)
-	if ( CLIENT ) then return end
+	if (CLIENT) then return end
 	local npc=args[1]
 	local ent=ents.Create(npc or "nextbot_monster")
 	ent:Activate()
 	ent:Spawn()
 	
-	ent:SetPos( ply:GetEyeTrace().HitPos )
+	ent:SetPos(ply:GetEyeTrace().HitPos)
 end
-concommand.Add( "mh_spawn_npc", mh.CreateNpc )
+concommand.Add("mh_spawn_npc", mh.CreateNpc)
